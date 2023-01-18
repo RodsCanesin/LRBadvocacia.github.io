3bf5545d165c2250cb30799417340c9d6e5057c7
RodsCanesin <rodrigocanesin90@gmail.com>
2023-01-18 15:52:17 -0300

    inserido modal

---
+++ js/modal.js
@@ -0,0 +1,23 @@
+function abrirModal(carregarModal) {
+  let modal = document.getElementById(carregarModal);
+  modal.style.display = 'block';
+  document.body.style.overflow = 'hidden';
+}
+
+function fecharModal(fecharModal) {
+  let modal = document.getElementById(fecharModal);
+  modal.style.display = 'none';
+  document.body.style.overflow = 'auto';
+}
+
+function abrirModal2(carregarModal2) {
+  let modal2 = document.getElementById(carregarModal2);
+  modal2.style.display = 'block';
+  document.body.style.overflow = 'hidden';
+}
+
+function fecharModal2(fecharModal2) {
+  let modal2 = document.getElementById(fecharModal2);
+  modal2.style.display = 'none';
+  document.body.style.overflow = 'auto';
+}
